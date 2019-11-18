df5c059b16f6c19f023a067e482a53376b146175
OlegKrikun <oleg.krikun@gmail.com>
2019-11-19 00:07:52 +0300

    Update Kotlin to 1.3.60

--- commonmark-ktx/build.gradle.kts
+++ commonmark-ktx/build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.50")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.60")
     implementation("com.atlassian.commonmark:commonmark:0.13.0")
 }
