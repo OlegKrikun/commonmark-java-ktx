cd9a3bff2976ef9febe3d5865220e15b84f49e7a
OlegKrikun <oleg.krikun@gmail.com>
2020-01-18 22:03:21 +0300

    Provide commonmark-java dependencies to commonmark-java-ktx consumers

--- commonmark-ktx/build.gradle.kts
+++ commonmark-ktx/build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.61")
-    implementation("com.atlassian.commonmark:commonmark:0.13.1")
+    api("com.atlassian.commonmark:commonmark:0.13.1")
 }
